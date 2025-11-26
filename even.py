e3456f0414786b29af42866d1c860976cda2d2c5
saii-v <venkats23usa@gmail.com>
2025-11-25 21:37:43 -0600

    modified even.py and added scientific

--- even.py
+++ even.py
@@ -2,8 +2,8 @@
 
 num = int(input("Enter a number: "))
 
-if num % 2 == 0:
-    print(num, "is an even number")
-else:
+if num % 3 == 0:
     print(num, "is an odd number")
+else:
+    print(num, "is an even number")
 
